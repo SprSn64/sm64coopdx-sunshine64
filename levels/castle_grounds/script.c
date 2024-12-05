f5c4fbbf4b9d7db12c3399e566774907f0addaef
SprSn64 <136403175+SprSn64@users.noreply.github.com>
2024-12-05 20:31:31 +1100

    bowl of wet slop
    plus an actual sunshine object

---
+++ levels/castle_grounds/script.c
@@ -0,0 +1,54 @@
+#include <ultra64.h>
+#include "sm64.h"
+#include "behavior_data.h"
+#include "model_ids.h"
+#include "seq_ids.h"
+#include "dialog_ids.h"
+#include "segment_symbols.h"
+#include "level_commands.h"
+
+#include "game/level_update.h"
+
+#include "levels/scripts.h"
+
+
+/* Fast64 begin persistent block [includes] */
+/* Fast64 end persistent block [includes] */
+
+#include "make_const_nonconst.h"
+#include "levels/castle_grounds/header.h"
+
+/* Fast64 begin persistent block [scripts] */
+/* Fast64 end persistent block [scripts] */
+
+const LevelScript level_castle_grounds_entry[] = {
+	INIT_LEVEL(),
+	LOAD_MIO0(0x7, _castle_grounds_segment_7SegmentRomStart, _castle_grounds_segment_7SegmentRomEnd), 
+	LOAD_MIO0(0xa, _bits_skybox_mio0SegmentRomStart, _bits_skybox_mio0SegmentRomEnd), 
+	ALLOC_LEVEL_POOL(),
+	MARIO(MODEL_MARIO, 0x00000001, bhvMario),
+
+	/* Fast64 begin persistent block [level commands] */
+	/* Fast64 end persistent block [level commands] */
+
+	AREA(1, castle_grounds_area_1),
+		WARP_NODE(0x0A, LEVEL_CASTLE_COURTYARD, 0x01, 0x0A, WARP_NO_CHECKPOINT),
+		OBJECT(MODEL_NONE, 0, 80, 0, 0, 180, 0, 0x000A0000, bhvInstantActiveWarp),
+		MARIO_POS(0x01, 180, 0, 80, 0),
+		OBJECT(E_MODEL_FLUDDPICKUP, 2388, -609, -1323, 0, 63, 0, 0x00000000, id_bhvfluddPickup),
+		TERRAIN(castle_grounds_area_1_collision),
+		MACRO_OBJECTS(castle_grounds_area_1_macro_objs),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_MENU_FILE_SELECT),
+		TERRAIN_TYPE(TERRAIN_GRASS),
+		/* Fast64 begin persistent block [area commands] */
+		/* Fast64 end persistent block [area commands] */
+	END_AREA(),
+
+	FREE_LEVEL_POOL(),
+	MARIO_POS(0x01, 180, 0, 80, 0),
+	CALL(0, lvl_init_or_update),
+	CALL_LOOP(1, lvl_init_or_update),
+	CLEAR_LEVEL(),
+	SLEEP_BEFORE_EXIT(1),
+	EXIT(),
+};
--- main.lua
+++ main.lua
@@ -1,2 +1,27 @@
 -- name: Mario Sunshine 64
 -- description: i have no idea what im doing
+
+E_MODEL_FLUDDPICKUP = smlua_model_util_get_id("fluddpickup_geo")
+E_MODEL_FLUDD = smlua_model_util_get_id("fludd_geo")
+
+local objSpawned = false
+
+local function update()
+	
+end
+
+local function on_level_init()
+
+end
+
+function fluddPickup_init(obj)
+end
+
+function fluddPickup_loop(obj)
+	obj.oPosY = 0
+end
+
+hook_event(HOOK_UPDATE, update)
+hook_event(HOOK_ON_LEVEL_INIT, on_level_init)
+
+id_bhvfluddPickup = hook_behavior(nil, OBJ_LIST_DEFAULT, false, fluddPickup_init, fluddPickup_loop, "bhvfluddPickup")
